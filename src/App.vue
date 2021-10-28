3e7417f6357caf5ef11eb8a7c0d73c0fd2fe2b77
MatheusDLM <47402274+MatheusDLM@users.noreply.github.com>
2021-10-27 23:04:26 -0300

    Testes

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+
+      <h1>Bem vindo ao meu primeiro projeto!</h1>
+      <button @click="entrar">Entrar</button>
+
+      <Usuario nome="Fi"/>
+      <Usuario :nome="nome"/>
+
+  </div>
+  
+</template>
+
+<script>
+import Usuario from './components/Usuario';
+  export default {
+    name: 'App',
+    components: {
+      Usuario
+    },
+    data(){
+      return {
+        nome: 'Sujeito programador'
+      }
+    },
+    methods:{
+      entrar(){
+        this.nome = 'Curso Vue Js';
+      }
+    }
+  }
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/Usuario.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="usuario">
+
+        <h2>Olá {{nome}}</h2>
+
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Usuario',
+    props: {
+        nome: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h2{
+        color:red;
+    }
+
+</style>
